70f5731843f08ff9182964b5054c978784273c72
chetan9423 <126280755+chetan9423@users.noreply.github.com>
2025-04-17 07:48:24 +0530

    Create Helloword.cs

---
+++ Helloword.cs
@@ -0,0 +1,13 @@
+using System;
+class HelloWord
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Hello, World.");
+      string input = Console.ReadLine();
+        
+        // Print user input
+        Console.WriteLine(input);
+    }
+}
