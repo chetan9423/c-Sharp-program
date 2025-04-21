f8b505e8faf57cdc881b0a72406bc588251c001c
chetan9423 <126280755+chetan9423@users.noreply.github.com>
2025-04-21 16:57:35 +0530

    Create EvenOdd.cs

---
+++ EvenOdd.cs
@@ -0,0 +1,19 @@
+using System;
+class EvenOddd
+{
+static void Main()
+{
+Console.Write("Enter the number ");
+int i=int.Parse(Console.ReadLine());
+if(i % 2==0)
+{
+Console.WriteLine("This Number Is Even");
+}
+else
+{
+Console.WriteLine("This Number is Odd");
+
+}
+
+}
+}
