5d48263eafa001088a2177e03d871456557675bb
chetan9423 <126280755+chetan9423@users.noreply.github.com>
2025-04-24 19:19:14 +0530

    Create PrimeComposite.cs

---
+++ PrimeComposite.cs
@@ -0,0 +1,34 @@
+using System;
+class PrimeNumber
+{
+static void Main()
+{
+Console.WriteLine("Enter the number");
+uint Num=uint.Parse(Console.ReadLine());
+
+if(Num==0 || Num==1)
+{
+Console.WriteLine("Enter the other number 0 and 1");
+return;
+}
+bool isPrime=true;
+uint HalfNum=Num/2;
+for(uint i=2;i<=HalfNum;i++)
+{
+if(Num % i ==0)
+{
+isPrime=false;
+break;
+}
+}
+if(isPrime)
+Console.WriteLine("This is Prime number");
+else
+Console.WriteLine("This is Composite number");
+Console.WriteLine();
+
+
+
+
+}
+}
